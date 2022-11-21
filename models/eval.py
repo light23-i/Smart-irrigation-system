d2e24c6d6105e00348b166630b01ea893e35977d
light23-i <kshitijgera785@gmail.com>
2022-11-21 16:40:59 +0530

    new

--- models/eval.py
+++ models/eval.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt 
 
-def plot(train_loss,val_loss):
-    epochs = range(1,len(train_loss) + 1)
+def plot(train_ac,val_ac):
+    epochs = range(1,len(train_ac) + 1)
     fig,ax = plt.subplots(1)
-    ax.plot(epochs,train_loss)
-    ax.plot(epochs,val_loss)
-    ax.set(xlabel='Epochs',ylabel='Loss')
+    ax.plot(epochs,train_ac)
+    ax.plot(epochs,val_ac)
+    ax.set(xlabel='Epochs',ylabel='Accuracy')
     plt.show()
     fig.savefig('plot.png')
     return 
